a734e7f42aa27529eecd5661c04271fab6b03270
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2024-08-30 22:06:54 +0700

    Update BetaFast.lua

--- BetaFast.lua
+++ BetaFast.lua
@@ -54,22 +54,26 @@ task.spawn(function()
 	local old = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
 	local com = debug.getupvalue(old, 2)
 	require(game.ReplicatedStorage.Util.CameraShaker):Stop()
-	while task.wait(_G.FastAttackDelay) do 
-		task.spawn(pcall,function()
-			if not getgenv().CurrentCharHum or not getgenv().CurrentCharHum.Parent or getgenv().CurrentCharHum.ClassName ~= 'Humanoid' then
-				getgenv().CurrentCharHum = game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
-			end
-			com.activeController.hitboxMagnitude = 60
-			if (_G.UseFAttack) and (getgenv().CurrentCharHum and getgenv().CurrentCharHum.Parent.Stun.Value <= 0) then
-				com.activeController.hitboxMagnitude = 60
-				com.activeController.active = false
-				com.activeController.blocking = false
-				com.activeController.focusStart = 0
-				com.activeController.hitSound = nil
-				com.activeController.increment = 0
-				com.activeController.timeToNextAttack = 0
-				com.activeController.timeToNextBlock = 0
-				com.activeController:attack()
+	if not _G.FastAttackConnect then 
+		_G.FastAttackConnect = task.spawn(function()
+			while task.wait(_G.FastAttackDelay) do 
+				task.spawn(pcall,function()
+					if not getgenv().CurrentCharHum or not getgenv().CurrentCharHum.Parent or getgenv().CurrentCharHum.ClassName ~= 'Humanoid' then
+						getgenv().CurrentCharHum = game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+					end
+					com.activeController.hitboxMagnitude = 60
+					if (_G.UseFAttack) and (getgenv().CurrentCharHum and getgenv().CurrentCharHum.Parent.Stun.Value <= 0) then
+						com.activeController.hitboxMagnitude = 60
+						com.activeController.active = false
+						com.activeController.blocking = false
+						com.activeController.focusStart = 0
+						com.activeController.hitSound = nil
+						com.activeController.increment = 0
+						com.activeController.timeToNextAttack = 0
+						com.activeController.timeToNextBlock = 0
+						com.activeController:attack()
+					end
+				end)
 			end
 		end)
 	end
